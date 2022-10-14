38ebb30b31a2372d480b0e7b0e010aab3fe60ca3
Jakub Klimczewski <kukli8@o2.pl>
2022-10-14 09:33:11 +0200

    lab02changes

--- lab02/zad02.py
+++ lab02/zad02.py
@@ -6,18 +6,15 @@ ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
 ssh.connect("sigma.ug.edu.pl")
 
-name = input("Which file: ")
+# name = input("Which file: ")
+name = "/home/jklimczewski/test.txt"
+stdin, stdout, stderr = ssh.exec_command(command + " " + name)
+if not stderr.readline():
+    # lines = stdout.readline()
+    # print(lines)
+    stdin2, stdout2, stderr2 = ssh.exec_command("cat" + " " + name)
+    print(stdout2.readline())
+else:
+    print("err")
 
-stdin, stdout, stderr = ssh.exec_command(command)
-lines = stdout.readlines()
-print(lines)
-print(lines[12])
-for nazwa in lines:
-    if str(name) == str(nazwa):
-        print(nazwa)
-        print("essa")
-    else:
-        print(nazwa)
-        print(name)
-        print("no")
 ssh.close()
