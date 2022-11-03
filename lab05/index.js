af0bc2419a9b8f3129544596ebc8ba4e5f06ac68
Jakub Klimczewski <kukli8@o2.pl>
2022-11-03 17:29:13 +0100

    lab05-firstcommit

---
+++ lab05/index.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     res.send("Get request appearing\n");
+// });
+
+// app.post('/', (req, res) => {
+//     res.send("Posting appearing\n");
+// })
+
+// app.listen(3001);
+
+const plansza = [
+    [ '', ' | ', '', ' | ', '' ],
+    ['-------------'],
+    [ '', ' | ', '', ' | ', '' ],
+    ['-------------'],
+    [ '', ' | ', '', ' | ', '' ]
+]
+console.log(plansza)
+//wstawiając znak możemy pytać 0, 1, 2 i mnozyc razy 2
